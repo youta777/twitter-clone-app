1488efd240495f64bb03c858c4e3e107f4170ebd
清水耀太 <yousoccer07@yahoo.co.jp>
2020-03-28 18:43:19 +0900

    ユーザ登録確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  # ユーザ登録確認
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash[:danger] = 'ユーザの登録が失敗しました。'
+      render :new
+    end
   end
+end
+
+private
+
+# Strong Parameter
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation)
 end
